884bb01e7c3be18cbe5366b5418c9fceae9d3657
albertoramos98 <albertinhorss@gmail.com>
2026-04-05 14:32:58 -0300

    testeultimo

--- js/lib/supabase.js
+++ js/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'sb_publishable_LyMrellyhNlFpSOTBeMUOA_SSwVrdZo';
+const SUPABASE_URL = 'https://cmmzqprrqdopypseyciz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtbXpxcHJycWRvcHlwc2V5Y2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzOTkzMzEsImV4cCI6MjA5MDk3NTMzMX0.Thm9qThTd0hZWzNIwW9REZLEZ9-A39Fbr_JFb-HpX48';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
  (function() {
          // Ajustado: Adicionado o "n" em "cmmzqprrrqdopypsecyizn"
-         const URL = 'https://cmmzqprrrqdopypsecyizn.supabase.co';
+         const URL = 'https://cmmzqprrqdopypseyciz.supabase.co';
          const KEY =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtbXpxcHJycWRvcHlwc2V5Y2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzOTkzMzEsImV4cCI6MjA5MDk3NTMzMX0.Thm9qThTd0hZWzNIwW9REZLEZ9-A39Fbr_JFb-HpX48';
       
